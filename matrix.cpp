d91cc3264624caff2a2a5ac209ff7db5176f8574
Somesthetic <forest-denton@ku.edu>
2025-04-09 22:17:45 -0500

    all the matrix.cpp and .hpp stuff is configured

--- matrix.cpp
+++ matrix.cpp
@@ -7,8 +7,8 @@
 // the class name and "::"
 
 // e.g. for the constructor:
-
-Matrix::Matrix(size_t N) {
+template <class T>
+Matrix<T>::Matrix(size_t N) {
   for (int row = 0; row <= size-1; row++){
     std::vector<int> new_row;
     for (int column = 0; column <= size-1; column++){
@@ -18,18 +18,19 @@ Matrix::Matrix(size_t N) {
   }
 }
 
-Matrix::Matrix(std::vector<std::vector<int>> nums){;
+template <class T>
+Matrix<T>::Matrix(std::vector<std::vector<T>> nums){;
   size = nums.size();
   for (int row = 0; row <= size-1; row++){
-    std::vector<int> new_row;
+    std::vector<T> new_row;
     for (int column = 0; column <= size-1; column++){
       new_row.push_back(nums[row][column]);  // adds individual value to end of column
     }
     the_data.push_back(new_row);
   }
 }
-
-void Matrix::print_matrix() const {
+template <class T>
+void Matrix<T>::print_matrix() const {
     for (int row = 0; row< size-1; row++){
       for (int column = 0; column < size-1; column++){
         std::cout<< the_data[row][column]<< " ";  // prints each value in the column, followed by a space
@@ -38,22 +39,25 @@ void Matrix::print_matrix() const {
     }
 }
 
-
-int Matrix::get_size() const{
+template <class T>
+int Matrix<T>::get_size() const{
   return size;  // Used to still have access to size without making it public
 }
 
-int Matrix::get_value(std::size_t i, std::size_t j) const{;
+template <class T>
+T Matrix<T>::get_value(std::size_t i, std::size_t j) const{;
   return the_data[i][j];
 }
 
-void Matrix::set_value(std::size_t i, std::size_t j, int n){;
+template <class T>
+void Matrix<T>::set_value(std::size_t i, std::size_t j, T n){;
   the_data[i][j] = n;  // returns val at that coord
   return;
 }
 
-Matrix Matrix::operator + (const Matrix &rhs) const{;
-  Matrix output(size);
+template <class T>
+Matrix<T> Matrix<T>::operator + (const Matrix &rhs) const{;
+  Matrix<float> output(size);  // Makes sure the output is always a float
   for (int i = 0; i <= size-1; i++){;
     for (int j = 0; j <= size-1; j++){;
       output.set_value(i,j, get_value(i,j) + rhs.get_value(i,j));
@@ -62,8 +66,9 @@ Matrix Matrix::operator + (const Matrix &rhs) const{;
   return output;
 }
 
-Matrix Matrix::operator*(const Matrix &rhs) const{;
-  Matrix output(size);
+template <class T>
+Matrix<T> Matrix<T>::operator*(const Matrix &rhs) const{;
+  Matrix<float> output(size);
   for (int i = 0; i < size; i++) {           // Iterate over rows of mat1
     for (int j = 0; j < size; j++) {       // Iterate over columns of mat2
       for (int k = 0; k < size; k++) {   // Iterate over row elements of mat1 and column elements of mat2
@@ -74,7 +79,8 @@ Matrix Matrix::operator*(const Matrix &rhs) const{;
   return output;
 }
 
-int Matrix::sum_diagonal_major() const{;
+template <class T>
+T Matrix<T>::sum_diagonal_major() const{;
   int sum = 0;
   for (int i = 0; i < size; i++){;
     sum += get_value(i,i);
@@ -82,7 +88,8 @@ int Matrix::sum_diagonal_major() const{;
   return sum;
 }
 
-int Matrix::sum_diagonal_minor() const{;
+template <class T>
+T Matrix<T>::sum_diagonal_minor() const{;
   int sum = 0;
   for (int i = 0; i < size; i++){;
     sum += get_value(i,size-1-i);
@@ -90,7 +97,8 @@ int Matrix::sum_diagonal_minor() const{;
   return sum;
 }
 
-void Matrix::swap_rows(std::size_t r1, std::size_t r2){;
+template <class T>
+void Matrix<T>::swap_rows(std::size_t r1, std::size_t r2){;
   std::vector temp_row = the_data[r1];
   for (int j = 0; j < size; j++){;
     set_value(r1, j, the_data[r2][j]);  // setting the r1 variables
@@ -99,8 +107,9 @@ void Matrix::swap_rows(std::size_t r1, std::size_t r2){;
   }
 }
 
-void Matrix::swap_cols(std::size_t c1, std::size_t c2){;
-  std::vector<int> temp_column;
+template <class T>
+void Matrix<T>::swap_cols(std::size_t c1, std::size_t c2){;
+  std::vector<T> temp_column;
   for (int i = 0; i < size; i++){;
     temp_column[i] = get_value(i, c1);  // get the value about to be deleted
     set_value(i, c1, get_value(i, c2));  // replaces the c1 value
